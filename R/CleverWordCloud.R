593efd99b5a4ba5517a6a3d7e51c09b9870501cf
Victor Francis <francisvictor47@gmail.com>
2022-01-19 02:38:34 -0800

    Add CleverWordCloud()

--- R/CleverWordCloud.R
+++ R/CleverWordCloud.R
@@ -2,7 +2,7 @@
 #'
 #' @param text Input an array of text / strings
 #' @param stopwords Input an set of strings
-#' @param shape Input a string that represents the path to an image
+#' @param shape Input a string that represents the path to an image.
 #' @param max_words Input an integer to indicate the maximum number of words included in the word cloud
 #'
 #' @return An image

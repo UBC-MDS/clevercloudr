eb670cf2a2917522ee9ea54cd9075033dcfc19be
Amelia Tang <amelia9@student.ubc.ca>
2022-01-19 02:07:33 -0800

    LemStem Roxygen2

---
+++ R/CleverLemStem.R
@@ -0,0 +1,12 @@
+#' This function conducts lemmatinzation and stemming on a string.
+#'
+#' @param text Input a string
+#'
+#' @return a lemmatized and stemmed string
+#' @export
+#'
+#' @examples
+#' CleverLemStem("crying feet maximum")
+CleverLemStem <- function(text) {
+
+}

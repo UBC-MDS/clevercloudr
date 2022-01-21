2c77494ef28c14427428b09595e94a76f4645743
Victor Francis <francisvictor47@gmail.com>
2022-01-20 19:32:13 -0800

    Update CleverWordCloud.R

--- R/CleverWordCloud.R
+++ R/CleverWordCloud.R
@@ -1,9 +1,9 @@
 #' This function generates a visually appealing word cloud with customized shape and stopwords.
 #'
-#' @param text Input an array of text / strings
+#' @param text Input an array of text / strings.
 #' @param stopwords Input an set of strings.
 #' @param shape Input a string that represents the path to an image.
-#' @param max_words Input an integer to indicate the maximum number of words included in the word cloud
+#' @param max_words Input an integer to indicate the maximum number of words included in the word cloud.
 #'
 #' @return An image
 #' @export

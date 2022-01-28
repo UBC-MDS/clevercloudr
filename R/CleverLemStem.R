f0004f30928845082c5f1c173ac3b5e705f3c934
Amelia Tang <amelia9@student.ubc.ca>
2022-01-28 01:12:20 -0800

    Add test for CleverLemStem and add porter stemmer in the function

--- R/CleverLemStem.R
+++ R/CleverLemStem.R
@@ -13,14 +13,14 @@ library(textstem)
 CleverLemStem <- function(text) {
 
   if(!is.list(text)){
-   stop("The input is not a list!")
-   }
+    stop("The input is not a list!")
+  }
 
   for (i in text){
     if(typeof(i) != "character"){stop("Each element of the input list should be a string/character(s)")}
   }
   text <- as.vector(text)
   lem_text <- lemmatize_words(text)
-  stem_lem_text <- stem_strings(lem_text)
+  stem_lem_text <- stem_strings(lem_text, language = "porter")
   return(stem_lem_text)
 }

86dcbc75cc6a5486a66df3caacdfbb3e747b87e0
Victor Francis <francisvictor47@gmail.com>
2022-01-27 19:25:33 -0800

    Update CleverWordCloud.R function

--- R/CleverWordCloud.R
+++ R/CleverWordCloud.R
@@ -1,15 +1,14 @@
 #' This function generates a visually appealing word cloud with customized shape and stopwords.
 #'
 #' @param text Input an array of text / strings.
-#' @param stopwords Input an set of strings.
-#' @param shape Input a string that represents the path to an image.
-#' @param max_words Input an integer to indicate the maximum number of words included in the word cloud
+#' @param CleverStop Input an set of strings.
+#' @param max_w Input an integer to indicate the maximum number of words included in the word cloud
 #'
 #' @return An image
 #' @export
 #'
 #' @examples
-#' CleverWordCloud(sample_text, max_words=200)
-CleverWordCloud <- function(text, stopwords=STOPWORDS, shape=CLOUD, max_words=100) {
+#' CleverWordCloud(text, {"are", "my", "is"}, 200)
+CleverWordCloud <- function(text, CleverStop, max_w) {
 
 }

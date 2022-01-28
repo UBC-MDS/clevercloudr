348adf81492aef77491536e4f12d071af381b9bf
Victor Francis <francisvictor47@gmail.com>
2022-01-28 05:38:54 -0800

    Add unit test 2 to test-CleverWordCloud.R file

--- tests/testthat/test-CleverWordCloud.R
+++ tests/testthat/test-CleverWordCloud.R
@@ -2,3 +2,8 @@ test_that("Non-dataframe input value for CleverWordCloud should throw an error",
   expect_error(CleverWordCloud(2))
   expect_error(CleverWordCloud(list(4)))
 })
+
+test_that("String input value for CleverWordCloud should throw an error", {
+  expect_error(CleverWordCloud("Victor Amelia Arushi Adrianne"))
+  expect_error(CleverWordCloud('gdhs, fgfhj, dgsj'))
+})

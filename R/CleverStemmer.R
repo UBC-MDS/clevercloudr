710b3e9c3081cf9d2f9c1c31f7ed97cffc09c237
Amelia Tang <amelia9@student.ubc.ca>
2022-01-28 18:29:38 -0800

    add new stemmer function

--- R/CleverStemmer.R
+++ R/CleverStemmer.R
@@ -1,22 +1,23 @@
-#' This function performs stemming on a string and converts it into a character vector
+#' This function performs stemming on a string or a character vector and converts it into a character vector
 #' @name CleverStemmer
 #'
-#' @param text Input a string
+#' @param text Input a string or a character vector
 #'
 #' @return a character vector that has been stemmed
 #' @export
 #'
 #' @examples
 #' CleverStemmer("running buying feeding")
+#' CleverStemmer(c("running", "buying", "feeding"))
 
+library(corpus)
 
 CleverStemmer<- function(text) {
 
+  # Check is the input is a string or a character vector
   if(!is.character(text)){
-    stop("The input is not a string!")
+    stop("The input is not a string or character vector!")
   }
-
-  library(corpus)
 
   stem_text <- corpus::text_tokens(text, stemmer = "en")
   output_text <- unlist(stem_text)
